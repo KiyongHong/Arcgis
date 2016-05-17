8a5115faf0cc5b24bd57bcb3f1a1c062ad7440f4
HKY <starmonhong@gmail.com>
2016-05-17 05:55:06 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arcgis::Application.config.secret_key_base = 'c9141f429486a0b78cc3277f49e420301072926fd7c3609a47c035bf6b8319e787d39aa2a90848338550f6eafe9af8d9daa6a82f917e581b098a3b16b85693ce'
